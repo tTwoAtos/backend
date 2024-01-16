0fc0021e6f7a037f6d9b1472a163fed0959889b3
Guillaume <guillaumehernandez047@gmail.com>
2024-01-16 14:49:54 +0100

    Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import databaseConfig from "./configs/database.config";
+import { StockController } from "./stock/stock.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [databaseConfig],
+            envFilePath: [".env"]
+        })
+    ],
+    controllers: [AppController, StockController],
+    providers: [AppService]
 })
 export class AppModule {}
